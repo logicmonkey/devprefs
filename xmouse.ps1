985089121c4c74c5b9815cc9d05c89246b10cc91
Piers Barber <Piers.Barber@arm.com>
2018-08-21 09:26:20 +0100

    xmouse.ps1 is a powershell script that adds sloppy focus to Windows10

---
+++ xmouse.ps1
@@ -0,0 +1,12 @@
+# Taken from...
+# https://superuser.com/questions/954021/how-do-you-enable-focus-follows-mouse-in-windows-10
+
+$signature = @"
+[DllImport("user32.dll")]
+public static extern bool SystemParametersInfo(int uAction, int uParam, ref int lpvParam, int flags );
+"@
+
+$systemParamInfo = Add-Type -memberDefinition  $signature -Name SloppyFocusMouse -passThru
+
+[Int32]$newVal = 1
+$systemParamInfo::SystemParametersInfo(0x1001, 0, [REF]$newVal, 2)
